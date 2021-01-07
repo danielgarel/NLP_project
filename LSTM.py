b4ba9f836c21bc8c71597c3bc3e581b5d2c61e7a
danielgarel <56274607+danielgarel@users.noreply.github.com>
2021-01-07 15:54:32 +0100

    LSTM

---
+++ LSTM.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+random_SEED = 11
+MAX_TRUNC_LEN = 350
+
+'''Load the clean datasets'''
+question1 = pd.read_csv('C:\\Users\\danie\\PycharmProjects\\NLP_project\\data\\corpus\\question1_sub_bal.clean.txt', sep="\n", header=None)
+question2 = pd.read_csv('C:\\Users\\danie\\PycharmProjects\\NLP_project\\data\\corpus\\question2_sub_bal.clean.txt', sep="\n", header=None)
+question1.columns = ['question']
+question2.columns = ['question']
+
+'''Load the train-test split'''
+doc_name_list = []
+doc_train_list = []
+doc_test_list = []
+
+with open('data/' + 'quora_split_balanced_subset' + '.txt', 'r') as f:
+    for line in f.readlines():
+        doc_name_list.append(line.strip())
+        temp = line.split("\t")
+
+        if temp[2].find('test') != -1:
+            doc_test_list.append(line.strip())
+        elif temp[2].find('train') != -1:
+            doc_train_list.append(line.strip())
+
+train_ids = []
+for train_name in doc_train_list:
+    train_id = doc_name_list.index(train_name)
+    train_ids.append(train_id)
+random.Random(random_SEED).shuffle(train_ids)
+
+test_ids = []
+for test_name in doc_test_list:
+    test_id = doc_name_list.index(test_name)
+    test_ids.append(test_id)
+random.Random(random_SEED).shuffle(test_ids)
+
+'''Apply Keras Tokenizer'''
+MAX_NB_WORDS = 200000
+tokenizer = Tokenizer(num_words = MAX_NB_WORDS)
+tokenizer.fit_on_texts(list(question1['question'].values.astype(str))+list(question2['question'].values.astype(str)))
+
+
+Xtrain_q1 = tokenizer.texts_to_sequences(question1.loc[train_ids]['question'].values.astype(str))
+X_train_q1 = pad_sequences(X_train_q1, maxlen=MAX_TRUNC_LEN, padding='post')
+
+X_train_q2 = tokenizer.texts_to_sequences(question2.loc[train_ids]['question'].values.astype(str))
+X_train_q2 = pad_sequences(X_train_q2, maxlen=MAX_TRUNC_LEN, padding='post')
+
+X_test_q1 = tokenizer.texts_to_sequences(question1.loc[test_ids]['question'].values.astype(str))
+X_test_q1 = pad_sequences(X_test_q1, maxlen=MAX_TRUNC_LEN, padding='post')
+
+X_test_q2 = tokenizer.texts_to_sequences(question2.loc[test_ids]['question'].values.astype(str))
+X_test_q2 = pad_sequences(X_test_q2, maxlen=MAX_TRUNC_LEN, padding='post')
+
+word_index =tokenizer.word_index
+
+# load pre-trained word embeddings
+word_embeddings_dim = 300
+embedding_index = {}
+
+with open('glove.6B.' + str(word_embeddings_dim) + 'd.txt', 'r') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vectors = np.asarray(values[1:], 'float32')
+        embedding_index[word] = vectors
+    f.close()
+
+embedding_matrix = np.random.random((len(word_index)+1, 200))
+for word, i in word_index.items():
+    embedding_vector = embedding_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+'''LSTM'''
+
+# Model for Q1
+model_q1 = tf.keras.Sequential()
+model_q1.add(Embedding(input_dim = len(word_index)+1,
+                       output_dim = 200,
+                      weights = [embedding_matrix],
+                      input_length = MAX_TRUNC_LEN))
+model_q1.add(LSTM(128, activation = 'tanh', return_sequences = True))
+model_q1.add(Dropout(0.2))
+model_q1.add(LSTM(128, return_sequences = True))
+model_q1.add(LSTM(128))
+model_q1.add(Dense(60, activation = 'tanh'))
+model_q1.add(Dense(2, activation = 'sigmoid'))
+
+# Model for Q2
+model_q2 = tf.keras.Sequential()
+model_q2.add(Embedding(input_dim = len(word_index)+1,
+                       output_dim = 200,
+                      weights = [embedding_matrix],
+                      input_length = 30))
+model_q2.add(LSTM(128, activation = 'tanh', return_sequences = True))
+model_q2.add(Dropout(0.2))
+model_q2.add(LSTM(128, return_sequences = True))
+model_q2.add(LSTM(128))
+model_q2.add(Dense(60, activation = 'tanh'))
+model_q2.add(Dense(2, activation = 'sigmoid'))
+
+# Merging the output of the two models,i.e, model_q1 and model_q2
+
+mergedOut = Multiply()([model_q1.output, model_q2.output])
+
+mergedOut = Flatten()(mergedOut)
+mergedOut = Dense(100, activation = 'relu')(mergedOut)
+mergedOut = Dropout(0.2)(mergedOut)
+mergedOut = Dense(50, activation = 'relu')(mergedOut)
+mergedOut = Dropout(0.2)(mergedOut)
+mergedOut = Dense(2, activation = 'sigmoid')(mergedOut)
+
+new_model = Model([model_q1.input, model_q2.input], mergedOut)
+new_model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy',
+                 metrics = ['accuracy'])
+history = new_model.fit([X_train_q1,X_train_q2],Y_train, batch_size = 2000, epochs = 10)
+
+y_pred = new_model.predict([X_test_q1, X_test_q2], batch_size=2000, verbose=1)
+y_pred += new_model.predict([X_test_q1, X_test_q2], batch_size=2000, verbose=1)
+y_pred /= 2
